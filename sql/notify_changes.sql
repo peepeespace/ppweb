6f0d53178698df8888fa55d29269d3d8142e2fe1
peepee <ppark9553@gmail.com>
2020-05-14 13:24:55 +0900

    트리거 하나로 합치기 최종 파일명은 notify_changes.sql

--- sql/notify_changes.sql
+++ sql/notify_changes.sql
@@ -12,3 +12,9 @@ BEGIN
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER userstate_changed
+AFTER INSERT OR UPDATE
+ON quant_userstate
+FOR EACH ROW
+EXECUTE PROCEDURE notify_userstate_changes()
