576413cee39ea88b7b183b93eb7ca8ca490ce012
peepee <ppark9553@gmail.com>
2020-05-14 12:45:51 +0900

    admin에 모델 모두 추가하기

--- quant/admin.py
+++ quant/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from quant.models import Ticker, MonitorStock, MinOHLCV
+from quant.models import Ticker, MonitorStock, MinOHLCV, UserState, PortHistory
 
 admin.site.register(Ticker)
 admin.site.register(MonitorStock)
 admin.site.register(MinOHLCV)
+admin.site.register(UserState)
+admin.site.register(PortHistory)
